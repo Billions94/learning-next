3fc8074def84377f0ede5de2dae6c463b6ddbd36
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-04-07 00:15:45 +0200

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Header.module.scss'
 
 export default function Header() {
-  return (
-    <header>
-        <h1 className={styles.title}>
-            <span>Web Development</span> News
-        </h1>
-    </header>
-  )
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>
+                <span>Web Development</span> News
+            </h1>
+            <p className={styles.description}>Keep up to date with new Web Development news</p>
+        </header>
+    )
 }
