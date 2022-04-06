d74a1e2551dec85ec82f6927107389470413d268
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-04-07 00:35:46 +0200

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import Head from 'next/head'
+import axios from 'axios'
 
-export default function Home() {
+export const getStaticProps = async () => {
+  try {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=10`)
+    const articles = data
+
+    return {
+      props: {
+        articles,
+      }
+    }
+  } catch (error) {
+    console.error('Error getting articles', error)
+  }
+}
+
+export default function Home({ articles }) {
+  console.log('The articles are: ', articles)
   return (
     <div>
       <Head>
