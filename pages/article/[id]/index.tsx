61365b0b41f2d044722153f4ba596b3594657b84
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-04-07 02:21:10 +0200

    commit

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,12 +1,13 @@
 import { useRouter } from "next/router"
 import { GetServerSidePropsContext, PreviewData } from "next/types"
 import { ParsedUrlQuery } from "querystring"
+import Link from "next/link"
 import { Articles, HomeProp } from "../.."
 import axios from "axios"
 
 
 
-export const getServerSideProps = async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
+export const getStaticProps = async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
     try {
         const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
         const article: Articles = data
@@ -21,14 +22,37 @@ export const getServerSideProps = async (context: GetServerSidePropsContext<Pars
     }
 }
 
+export const getStaticPaths = async () => {
+    try {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        const articles: Articles[] = data
+
+        const idArr = articles.map((item: Articles) => item.id)
+        const paths = idArr.map((id) => ({ params: { id: id.toString()}}))
+
+        return {
+            paths,
+            fallback: false 
+        }
+
+    } catch (error) {
+        console.error(`Error getting static paths`, error)
+    }
+}
+
 export default function article({ article }: HomeProp) {
 
     const router = useRouter()
     const { id } = router.query
 
   return (
-    <div>
-        This is  article {article.id}
-    </div>
+    <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href="/">
+            Go back
+        </Link>
+    </>
   )
 }
