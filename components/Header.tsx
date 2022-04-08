b4dd449c40676861d6da0a80c3739835032478c5
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2022-04-08 20:20:28 +0200

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
+import { useRecoilValue } from 'recoil'
+import { darkModeState } from '../atoms'
 import styles from '../styles/Header.module.scss'
 
 export default function Header() {
+
+    const darkMode = useRecoilValue(darkModeState)
+    const check: boolean = darkMode === false
+
     return (
-        <header className={styles.header}>
+        <header id={check ? styles.header : styles.headerDark}>
             <h1 className={styles.title}>
                 <span>Web Development</span> News
             </h1>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,20 @@
 import Navbar from './Navbar'
 import Header from './Header'
-import styles from '../styles/Layout.module.scss'
 import Meta from './Meta'
+import { darkModeState } from '../atoms'
+import { useRecoilValue } from 'recoil'
+import styles from '../styles/Layout.module.scss'
 
 export default function Layout({ children }) {
+
+    const darkMode = useRecoilValue(darkModeState)
+    const check: boolean = darkMode === false
+
     return (
         <>
             <Meta />
             <Navbar />
-            <div className={styles.container}>
+            <div id={check ? styles.container : styles.containerDark}>
                 <main className={styles.main}>
                     <Header />
                     {children}
